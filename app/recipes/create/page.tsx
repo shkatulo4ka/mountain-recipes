79dd0029784a16e5df43abab970e8c98029b2387
Shkatula Julia <shkatylo4ka@gmail.com>
2025-04-14 22:52:06 +0300

    feat: add select

--- app/recipes/create/page.tsx
+++ app/recipes/create/page.tsx
@@ -1,6 +1,20 @@
+import { prisma } from "@/app/utils/db";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Select, SelectItem, SelectTrigger, SelectContent, SelectValue  } from "@/components/ui/select";
+import { Textarea } from "@/components/ui/textarea";
 
-export default function CreateRecipe() {
+
+export default async function CreateRecipe() {
+    const categories = await prisma.category.findMany({
+        select:{
+            name: true,
+            id: true
+        }
+    });
+    console.log(categories)
+
     return(
         <div className="m-2">
             <Card>
@@ -9,7 +23,31 @@ export default function CreateRecipe() {
                     <CardDescription>Create your own recipe</CardDescription>
                 </CardHeader>
                 <CardContent>
-                    <form></form>
+                    <form className="flex flex-col gap-2">
+                        <div className="flex flex-col gap-2">
+                            <Label>Название</Label>
+                            <Input />
+                        </div>
+                        <div className="flex flex-col gap-2">
+                            <Label>Категория</Label>
+                            <Select>
+                                <SelectTrigger>
+                                    <SelectValue placeholder="Выбери категорию"/>
+                                </SelectTrigger>
+                                <SelectContent>
+                                    {categories.map(({id, name}) => (
+                                        <SelectItem key={id} value={name}>
+                                            {name}
+                                        </SelectItem>
+                                    ))}
+                                </SelectContent>
+                            </Select>
+                        </div>
+                        <div className="flex flex-col gap-2">
+                            <Label>Приготовление</Label>
+                            <Textarea />
+                        </div>
+                    </form>
                 </CardContent>
             </Card>
         </div>
