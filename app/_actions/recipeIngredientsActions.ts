bde9037bc865a3cb04b88526dd6a4532eaf38386
abr-ya <gamer05@list.ru>
2025-04-19 18:59:45 +0300

    feat: Add Recipe detail page and Ingridient dialog

--- app/_actions/recipeIngredientsActions.ts
+++ app/_actions/recipeIngredientsActions.ts
@@ -1,27 +1,31 @@
 "use server";
 
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { revalidatePath } from "next/cache";
+
 import { prisma } from "../utils/db";
 
 const { getUser } = getKindeServerSession();
 
-export async function createRecipeIngredientAction(formData: FormData, recipeId: string) {
-  const user = await getUser();
-  if (!user) return;
-
-  const ingredientId = formData.get("ingredientId");
-  const quantity = formData.get("quantity");
+export const addIngridientAction = async (formData: FormData) => {
+  const recipeID = formData.get("recipeId") as string;
+  const ingredientId = "842ba14b-7d2d-4cd7-a620-2744053e1673"; // todo: id from select!
+  const quantity = 30; // todo const quantity = formData.get("quantity");
+  const pathName = `/recipes/${recipeID}`;
 
   const data = await prisma.recipe_Ingredients.create({
     data: {
-      recipeID: recipeId as string,
-      ingredientId: ingredientId as string,
-      quantity: Number(quantity),
+      recipeID,
+      ingredientId,
+      quantity,
     },
   });
 
-  return;
-}
+  console.log("ingredient added", data);
+
+  revalidatePath(pathName);
+};
+
 export async function getRecipeIngredientsAction(recipeId: string) {
   const user = await getUser();
   if (!user) return;
