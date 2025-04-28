acb7c9f30d4c20d81469bd7d6b0ce2c3caf3bfef
abr-ya <gamer05@list.ru>
2025-04-28 15:54:02 +0300

    feature: Few updates (loading, no-data) on Recipe pages

--- app/recipes/page.tsx
+++ app/recipes/page.tsx
@@ -2,9 +2,9 @@ import Link from "next/link";
 import { buttonVariants } from "@/components/ui/button";
 import RecipeCard from "@/components/general/RecipeCard";
 import { getAllRecipesAction } from "../_actions/recipeActions";
+import { Suspense } from "react";
 
-const Recipes = async () => {
-  const data = await getAllRecipesAction();
+const Recipes = () => {
   return (
     <div className="py-6">
       <div className="flex items-center justify-between mb-4">
@@ -13,15 +13,24 @@ const Recipes = async () => {
           Create Recipe
         </Link>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grod-cols-3 gap-4">
-        {data.map((item) => (
-          <div key={item.id}>
-            <RecipeCard data={item} key={item.id} />
-          </div>
-        ))}
-      </div>
+      <Suspense fallback={<p>Loading...</p>}>
+        <AllRecipes />
+      </Suspense>
     </div>
   );
 };
+
+async function AllRecipes() {
+  const data = await getAllRecipesAction();
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grod-cols-3 gap-4">
+      {data.map((item) => (
+        <div key={item.id}>
+          <RecipeCard data={item} key={item.id} />
+        </div>
+      ))}
+    </div>
+  );
+}
 
 export default Recipes;
