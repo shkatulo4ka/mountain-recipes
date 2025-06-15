191f2b231b5b228e5c82ab52c129feadadc54795
abr-ya <gamer05@list.ru>
2025-06-15 07:32:22 +0300

    feature: Update DayEatings component

--- components/general/DayEatings.tsx
+++ components/general/DayEatings.tsx
@@ -5,16 +5,32 @@ interface IDayEatings {
   data: IEating[];
 }
 
+interface IEatingsList extends IDayEatings {
+  timeId: "0" | "1" | "2" | "3"; // todo: вынести и переиспользовать?
+}
+
+const EatingsList = ({ data, timeId }: IEatingsList) => (
+  <ul className="list-disc my-2 mx-5">
+    {data
+      .filter((el) => el.eatingTimeId === timeId)
+      .map((el) => (
+        <li key={el.id}>{el.recipe.name}</li>
+      ))}
+  </ul>
+);
+
+// todo: здесь можно мапать список приемов пищи и брать Id и название оттуда
+// данные из бд брать лучше - вдруг что-то переименуется или добавится? но пока mvp)
 const DayEatings = ({ data }: IDayEatings) => (
   <div className="w-[600px]">
     <Heading title="Завтрак" size="xl" />
-    {JSON.stringify(data.filter((el) => el.eatingTimeId === "1"))}
+    <EatingsList timeId={"1"} data={data} />
     <Heading title="Обед" size="xl" />
-    {JSON.stringify(data.filter((el) => el.eatingTimeId === "2"))}
+    <EatingsList timeId={"2"} data={data} />
     <Heading title="Ужин" size="xl" />
-    {JSON.stringify(data.filter((el) => el.eatingTimeId === "3"))}
+    <EatingsList timeId={"3"} data={data} />
     <Heading title="Перекусы" size="xl" />
-    {JSON.stringify(data.filter((el) => el.eatingTimeId === "0"))}
+    <EatingsList timeId={"0"} data={data} />
   </div>
 );
 
